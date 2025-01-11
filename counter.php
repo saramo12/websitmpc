fa6ce3113f602a071704001679db595e56993066
saramo123 <sarausama1001@gmail.com>
2025-01-11 12:39:14 +0200

    cs

---
+++ counter.php
@@ -0,0 +1,23 @@
+<?php
+// $file = 'counter.txt';
+
+// // Create the file if it doesn't exist
+// if (!file_exists($file)) {
+//     file_put_contents($file, 0);
+// }
+
+// // Read the current counter value
+// $counter = (int)file_get_contents($file);
+
+// // Increment the counter
+// $counter++;
+
+// // Write the updated value back to the file
+// file_put_contents($file, $counter);
+
+// // Return the updated counter value
+// echo $counter;
+
+echo"hello world";
+
+?>
