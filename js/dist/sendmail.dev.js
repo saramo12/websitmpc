54b7074dcf93c164273a50091bb533db21207125
saramo123 <sarausama1001@gmail.com>
2025-01-21 18:26:46 +0200

    up

--- js/dist/sendmail.dev.js
+++ js/dist/sendmail.dev.js
@@ -1,18 +1,5 @@
 "use strict";
 
-// function emailSend(){
-//     Email.send({
-//         Host : "smtp.mailendo.com",
-//         Username : "username",
-//         Password : "password",
-//         To : 'them@website.com',
-//         From : "you@isp.com",
-//         Subject : "This is the subject",
-//         Body : "And this is the body"
-//     }).then(
-//       message => alert(message)
-//     );
-// }
 function sendMail() {
   var params = {
     name: document.getElementById("name").value,
